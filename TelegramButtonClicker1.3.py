c30563b6828ee7a7c4f5f519386f4270838ab78b
BrainyGithub <108795900+BrainyGithub@users.noreply.github.com>
2022-12-11 15:13:22 +0100

    Add files via upload

--- TelegramButtonClicker1.3.py
+++ TelegramButtonClicker1.3.py
@@ -8,26 +8,43 @@ BOT_API_KEY = '5885749837:AAFrGe1YRIt7nmCLEvsidGfecoqKfVQS2Pk'
 def start(update, context):
     update.message.reply_text('Hello!')
 
-def pics(update, context):
+def Alex(update, context):
     # Create the buttons
-    button1 = InlineKeyboardButton("Button 1", callback_data="1")
-    button2 = InlineKeyboardButton("Button 2", callback_data="2")
-    button3 = InlineKeyboardButton("Button 3", callback_data="3")
-    button4 = InlineKeyboardButton("Button 4", callback_data="4")
-    button5 = InlineKeyboardButton("Button 5", callback_data="5")
-    button6 = InlineKeyboardButton("Button 6", callback_data="6")
+    button1 = InlineKeyboardButton("Maand 1", callback_data="1")
+    button2 = InlineKeyboardButton("Maand 2", callback_data="2")
+    button3 = InlineKeyboardButton("Maand 3", callback_data="3")
+    button4 = InlineKeyboardButton("Maand 4", callback_data="4")
+    button5 = InlineKeyboardButton("Maand 5", callback_data="5")
+    button6 = InlineKeyboardButton("Maand 6", callback_data="6")
+    button7 = InlineKeyboardButton("Maand 7", callback_data="7")
+    button8 = InlineKeyboardButton("Maand 8", callback_data="8")
+    button9 = InlineKeyboardButton("Maand 9", callback_data="9")
+    button10 = InlineKeyboardButton("Maand 10", callback_data="10")
+    button11 = InlineKeyboardButton("Maand 11", callback_data="11")
+    button12 = InlineKeyboardButton("Maand 12", callback_data="12")
+    button13 = InlineKeyboardButton("Maand 13", callback_data="13")
+    button14 = InlineKeyboardButton("Maand 14", callback_data="14")
+    button15 = InlineKeyboardButton("Maand 15", callback_data="15")
+    button16 = InlineKeyboardButton("Maand 16", callback_data="16")
+
+
 
     # Create a list of the buttons to create the keyboard
     keyboard = [
         [button1, button2, button3],
-        [button4, button5, button6]
+        [button4, button5, button6],
+        [button7, button8, button9],
+        [button10, button11, button12],
+        [button13, button14, button15],
+        [button16]
+
     ]
 
     # Create the keyboard markup
     keyboard_markup = InlineKeyboardMarkup(keyboard)
 
     # Send the message with the keyboard to the user
-    context.bot.send_message(chat_id=update.message.chat_id, text="Select a button:", reply_markup=keyboard_markup)
+    context.bot.send_message(chat_id=update.message.chat_id, text="Selecteer een maand:", reply_markup=keyboard_markup)
 
 def button_clicked(update, context):
     query = update.callback_query
@@ -57,7 +74,7 @@ def main():
 
     # Add command handler to start the bot and the command to display the keyboard
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("pics", pics))
+    dp.add_handler(CommandHandler("Alex", Alex))
 
     # Add callback query handler to handle button clicks
     dp.add_handler(CallbackQueryHandler(button_clicked))
